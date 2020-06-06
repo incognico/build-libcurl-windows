6577218ec8fc6bda3061ce4faab6811f390c6507
nico <nico@lifeisabug.com>
2020-06-06 21:42:09 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -45,6 +45,7 @@ set XIDEL="%CD%\bin\xidel\xidel.exe"
 REM Housekeeping
 %RM% -rf tmp_*
 %RM% -rf third-party
+%RM% -rf curl.zip
 %RM% -rf build_*.txt
 
 REM Get download url .Look under <blockquote><a type='application/zip' href='xxx'>
@@ -57,14 +58,14 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Download latest curl and rename to curl.zip
 echo Downloading latest curl...
-curl -C - "https://curl.haxx.se%url%" -o curl.zip
+curl "https://curl.haxx.se%url%" -o curl.zip
 
 REM Extract downloaded zip file to tmp_libcurl
 %SEVEN_ZIP% x curl.zip -y -otmp_libcurl | FIND /V "ing  " | FIND /V "Igor Pavlov"
 
 REM Get deps
-curl -C - "https://windows.php.net/downloads/php-sdk/deps/vs16/x64/zlib-1.2.11-vs16-x64.zip" -o tmp_zlib_x64.zip
-curl -C - "https://windows.php.net/downloads/php-sdk/deps/vs16/x86/zlib-1.2.11-vs16-x86.zip" -o tmp_zlib_x86.zip
+curl "https://windows.php.net/downloads/php-sdk/deps/vs16/x64/zlib-1.2.11-vs16-x64.zip" -o tmp_zlib_x64.zip
+curl "https://windows.php.net/downloads/php-sdk/deps/vs16/x86/zlib-1.2.11-vs16-x86.zip" -o tmp_zlib_x86.zip
 
 REM Extract deps
 %SEVEN_ZIP% x tmp_zlib_x64.zip -y -odeps_x64 | FIND /V "ing  " | FIND /V "Igor Pavlov"
@@ -74,6 +75,9 @@ cd tmp_libcurl\curl-*\winbuild
 
 REM set VCVERSION = 16
 set VCVERSION = 9
+
+cd /d "%ROOT_DIR%\tmp_libcurl\curl-*\lib"
+TYPE "%ROOT_DIR%\curl_disable.txt" >> setup-win32.h
 
 :buildnow
 
@@ -93,12 +97,12 @@ echo Compiling dll-release-x64 version...
 nmake /f Makefile.vc mode=dll VC=%VCVERSION% DEBUG=no GEN_PDB=no MACHINE=x64 WITH_DEVEL="%ROOT_DIR%/deps_x64" WITH_ZLIB=static
 
 REM Copy compiled *.dll files to third-party\lib\dll-release folder
-cd %ROOT_DIR%\tmp_libcurl\curl-*\builds\libcurl-vc-x86-release-*
+cd %ROOT_DIR%\tmp_libcurl\curl-*\builds\libcurl-vc-x86-release-dll-zlib-static-ipv6-sspi-winssl
 %MKDIR% -p %ROOT_DIR%\third-party\libcurl\lib\dll-release-x86
 %CP% bin\*.dll %ROOT_DIR%\third-party\libcurl\lib\dll-release-x86
 
 REM Copy compiled *.dll files to third-party\lib\dll-release folder
-cd %ROOT_DIR%\tmp_libcurl\curl-*\builds\libcurl-vc-x64-release-*
+cd %ROOT_DIR%\tmp_libcurl\curl-*\builds\libcurl-vc-x64-release-dll-zlib-static-ipv6-sspi-winssl
 %MKDIR% -p %ROOT_DIR%\third-party\libcurl\lib\dll-release-x64
 %CP% bin\*.dll %ROOT_DIR%\third-party\libcurl\lib\dll-release-x64
 
